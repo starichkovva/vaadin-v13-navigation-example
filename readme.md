79f4dfc8ece34dbf156f0200b8935818a60a64da
Vadim Starichkov <starichkovva@gmail.com>
2019-03-11 14:17:36 +0300

    Added more variants for navigation

--- readme.md
+++ readme.md
@@ -4,4 +4,17 @@ This example shows how to implement navigation between views in Vaadin using it'
 
 Also this example has a side menu, based on `FlexLayout`.
 
-Whole example is a `Spring Boot 2` application.
+Navigation implemented by using 2
+
+### Links
+
+Tutorials and posts used for this example:
+
+#### Official tutorials
+
+* [Router Layouts and Nested Router Targets](https://vaadin.com/docs/flow/routing/tutorial-router-layout.html)
+* [Navigating Between Routes](https://vaadin.com/docs/flow/routing/tutorial-routing-navigation.html)
+
+#### Vaadin Forum
+
+* [Navigation with Button](https://vaadin.com/forum/thread/17441436/navigation-with-button)
--- src/main/java/org/starichkov/java/vaadin/v13/navigation/ui/route/MenuBar.java
+++ src/main/java/org/starichkov/java/vaadin/v13/navigation/ui/route/MenuBar.java
@@ -1,6 +1,7 @@
 package org.starichkov.java.vaadin.v13.navigation.ui.route;
 
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.RouterLink;
 import org.starichkov.java.vaadin.v13.navigation.ui.layout.BaseRootLayout;
@@ -10,12 +11,20 @@ import org.starichkov.java.vaadin.v13.navigation.ui.layout.MainLayout;
 public class MenuBar extends BaseRootLayout {
 
   public MenuBar() {
-    addMenuElement(HomeView.class, "Home");
-    addMenuElement(OneMoreView.class, "One More");
-    addMenuElement(AnotherView.class, "Another");
-  }
+    // 1. Using the RouterLink Component
+    add(new RouterLink("Home", HomeView.class));
 
-  private void addMenuElement(Class<? extends Component> navigationTarget, String name) {
-    add(new RouterLink(name, navigationTarget));
+    // 2. Server-side Navigation
+
+    // found on forum, but not secure, UI.getCurrent() can return null
+    Button menuButtonOneMore = new Button("One More");
+    menuButtonOneMore.addClickListener(e -> UI.getCurrent().navigate(OneMoreView.class));
+    add(menuButtonOneMore);
+
+    // recommended way from official tutorial
+    Button menuButtonAnother = new Button("Another");
+    //menuButtonAnother.addClickListener(e -> menuButtonAnother.getUI().ifPresent(ui -> ui.navigate("another")));
+    menuButtonAnother.addClickListener(e -> menuButtonAnother.getUI().ifPresent(ui -> ui.navigate(AnotherView.class)));
+    add(menuButtonAnother);
   }
 }
